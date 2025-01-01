859d26b52dc2a74a77f880d3862bd8b4b0292c0f
Ihor Bahniuk <hattorikanto@gmail.com>
2025-01-02 00:35:17 +0200

    feat: created create-user DTO and added ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { env } from 'process';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   app.listen(env.PORT || 3000);
 };
 bootstrap();
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsEmail } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  nickname: string;
+
+  @IsString()
+  password: string;
+}
