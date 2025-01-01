bcfdb6ada0c6d9cc32ce328979b4a7bce0b262c6
Ihor Bahniuk <hattorikanto@gmail.com>
2025-01-02 00:13:25 +0200

    feat: connected CockroachDB with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const dbUrl = new URL(process.env.DATABASE_URL);
+
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'cockroachdb',
+      url: dbUrl.toString(),
+      ssl: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
